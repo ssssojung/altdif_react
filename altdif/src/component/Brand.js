7132d422e9b0bdfbc9629733d8b7fc0c483d6e53
sssojung <1216thwjd@naver.com>
2023-01-07 23:13:41 +0900

    brand 컨텐츠 추가

--- altdif/src/component/Brand.js
+++ altdif/src/component/Brand.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Brand(props) {
     return (
-        <section id='brand' >
-            브랜드 
+        <section id='brand'>
+            <div className='inner'>
+                <div className='textbox p-5 text-start'>
+                    <h2 className=''>We Bland Life</h2>
+                    <p>예술과 차가 있는 일상, 존엄성과 자유가 있는 삶, 그리고 다양성이 있는 사회<br/>
+                        <span>art, life, tea, dignity, diversity, freedom.</span><br/>
+                        알디프라는 이름에 담긴 우리의 자향점처럼,<br/>
+                        알디프는 세심하고 따뜻한 제품과 서비스를 통해<br/>
+                        더 나은 삶으로의 변화를 꿈꾸는 Tea & Lifestyle 브랜드입니다.
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
