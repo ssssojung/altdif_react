2c4f2b3b2a5b0848a1648ab2f008b9b1a630da69
sssojung <1216thwjd@naver.com>
2023-01-08 01:29:37 +0900

    play 컨텐츠 추가

--- altdif/src/component/Play.js
+++ altdif/src/component/Play.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import Playswiper from './Playswiper';
+import { Swiper, SwiperSlide} from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function Play(props) {
     return (
         <section id='play'>
-            플레이
+            <div className='d-flex p-5'>
+                <div className='col-12'>
+                <Swiper className="mySwiper"
+                    modules={[Pagination, Autoplay]}
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    pagination={{clickable: true,}}
+                    autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                    }}
+                >
+                    <SwiperSlide><Playswiper reI='playRe'/></SwiperSlide>
+                    <SwiperSlide>222</SwiperSlide>
+                    <SwiperSlide>222</SwiperSlide>
+                    <SwiperSlide>222</SwiperSlide>
+                    <SwiperSlide>222</SwiperSlide>
+                    <SwiperSlide>222</SwiperSlide>
+                    {/*  */}
+
+                </Swiper>
+                </div>
+                {/* <div>유튜브</div>
+                <div>기획시리즈</div>
+                <div>차취향테스트</div>
+                <div>인터뷰</div> */}
+            </div>
         </section>
     );
 }
 
 export default Play;
+{/* <Playswiper reI='playRe'/> */}
